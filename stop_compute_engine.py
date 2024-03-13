77e9a0fd9d956b66e1e0a06c0b640cdd346632c7
João Perez <63556203+peerezjoao@users.noreply.github.com>
2024-03-13 08:42:01 -0300

    Add files via upload

---
+++ stop_compute_engine.py
@@ -0,0 +1,49 @@
+from dotenv import load_dotenv
+from google.oauth2 import service_account
+from google.cloud import compute_v1
+import os, json, logging
+
+load_dotenv()
+
+CREDENTIALS = json.loads(os.environ.get('CREDENTIALS'))
+
+if not os.path.exists('credentials.json'):
+    with open('credentials.json', 'w') as cred_file:
+        json.dump(CREDENTIALS, cred_file)
+        
+def get_env_vars(var_name : str):
+    value = os.environ.get(var_name)
+    if not value:
+        raise ValueError(f'Required environment variable {var_name} is not set.' )
+    return value
+
+def stop_instance(credentials_json, project_id, carazone, instance_name):
+    logging.basicConfig(level=logging.INFO)
+    # validate essentials fields in credentials
+    if not all(field in credentials_json for field in ('project_id', 'private_key_id')):
+        raise ValueError(f"Missing required fields in credentials: project_id, private_key_id")
+
+    credentials_account = service_account.Credentials.from_service_account_info(credentials_json, 
+                                scopes=["https://www.googleapis.com/auth/cloud-platform"])
+    
+    try:
+        gce = compute_v1.InstancesClient(credentials=credentials_account)
+        request = compute_v1.StopInstanceRequest(project=project_id, zone=zone, instance=instance_name)
+        response = gce.stop(request)
+        logging.info(f'Instance {instance_name} stopped successfully. Response: {response}')
+
+    except Exception as e:
+        logging.error(f'Unexpected error: {e}')
+
+if __name__ == "__main__":
+    try:
+        # Retrieve and validate environment variables
+        project_id = CREDENTIALS['project_id']
+        zone = get_env_vars('ZONE')
+        instance_name = get_env_vars('MY_INSTANCE_NAME')
+
+        # Stop the instance 
+        stop_instance(CREDENTIALS, project_id, zone, instance_name)
+
+    except Exception as e:
+        logging.error(f"An error occurred in the main execution: {e}")
